bae7400e6256fc97f262626fd161c28a24a6bcd4
Andrew Wylde <andrew.wylde@dfinity.org>
2020-06-16 08:07:17 -0700

    add bootstrap

--- src/dfx_bootstrap_assets/public/index.js
+++ src/dfx_bootstrap_assets/public/index.js
@@ -1,5 +1,19 @@
 import dfx_bootstrap from 'ic:canisters/dfx_bootstrap';
+import dfx_bootstrap_assets from 'ic:canisters/dfx_bootstrap_assets';
+import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
 
+
+export const injectHtml = htmlBytes => {
+  const html = new TextDecoder().decode(htmlBytes);
+  const el = new DOMParser().parseFromString(html, "text/html");
+  document.body.replaceChild(el.firstElementChild, document.getElementById('app'));
+};
+
+dfx_bootstrap_assets.__getAsset('index.html').then(res => {
+  injectHtml(res);
+})
 dfx_bootstrap.greet(window.prompt("Enter your name:")).then(greeting => {
   window.alert(greeting);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const preCSS = require('precss');
+const autoPrefixer = require('autoprefixer');
 const dfxJson = require("./dfx.json");
 
 // List of all aliases for canisters. This creates the module alias for
 // the `import ... from "ic:canisters/xyz"` where xyz is the name of a
 // canister.
 const aliases = Object.entries(dfxJson.canisters)
-    .filter(([name,value]) => value.main)
-    .reduce((acc, [name,value]) => {
-  const outputRoot = path.join(__dirname, dfxJson.defaults.build.output, name);
-  const filename = path.basename(value.main, ".mo");
-  return {
-    ...acc,
-    ["ic:canisters/" + name]: path.join(outputRoot, filename + ".js"),
-    ["ic:idl/" + name]: path.join(outputRoot, filename + ".did.js"),
-  };
-}, {});
+  .reduce((acc, [name, value]) => {
+    const outputRoot = path.join(__dirname, dfxJson.defaults.build.output, name);
+    const canisterKey = "ic:canisters/" + name;
+    const idlKey = "ic:idl/" + name;
+    const canisterPath = path.join(outputRoot, name + ".js");
+    const idlPath = path.join(outputRoot, name + ".did.js");
+    console.log({ canisterKey, idlKey, canisterPath, idlPath })
+    return {
+      ...acc,
+      [canisterKey]: canisterPath,
+      [idlKey]: idlPath,
+    };
+  }, {});
+
 
 /**
  * Generate a webpack configuration for a canister.
@@ -40,13 +46,37 @@ function generateWebpackConfigForCanister(name, info) {
     },
     resolve: {
       alias: aliases,
+      extensions: [".ts", ".tsx", ".js"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(scss)$/,
+          use: [{
+            loader: 'style-loader', // inject CSS to page
+          }, {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          }, {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins: function () { // post css plugins, can be exported to postcss.config.js
+                return [
+                  preCSS,
+                  autoPrefixer
+                ];
+              }
+            }
+          }, {
+            loader: 'sass-loader' // compiles Sass to CSS
+          }]
+        }
+      ]
     },
     output: {
-      filename: "[name].js",
-      path: path.join(inputRoot, info.frontend.output),
+      filename: "index.js",
+      path: path.join(__dirname, info.frontend.output),
     },
-    plugins: [
-    ],
+    plugins: [],
   };
 }
 
